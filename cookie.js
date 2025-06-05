d6da411b66b8d1b70cdd429f1d4f58722f697777
mmmaaarrrccc <marcgarcia816@gmail.com>
2025-06-05 13:49:42 +0800

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -45,11 +45,10 @@ function cookiesEnabled() {
     console.log(backgroundImage);
     $('#bgLink').val(backgroundImage);
     $('body').css('background-image','url(' + backgroundImage + ')');
-    video.src = bgLink;
-    // video.play();
+    video.src = backgroundImage;
+    video.play();
   } else {
     $('body').css('background-image','none');   
-    // video.src = 'https://static.vecteezy.com/system/resources/previews/007/536/781/mp4/aerial-view-of-white-sand-beach-and-water-surface-texture-foamy-waves-with-sky-beautiful-tropical-beach-amazing-sandy-coastline-with-white-sea-waves-nature-seascape-and-summer-concept-free-video.mp4';
   }
 
   let backgroundSize = Cookies.get('backgroundSize');
