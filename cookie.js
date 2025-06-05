4472e2e6eede20e68da02212222a7c9a84ec4af3
mmmaaarrrccc <marcgarcia816@gmail.com>
2025-06-05 13:47:58 +0800

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -40,7 +40,7 @@ function cookiesEnabled() {
   }
 
   let backgroundImage = Cookies.get('backgroundImage');
-  // var video = $("#videoBg")[0];
+  var video = $("#videoBg")[0];
   if (backgroundImage != undefined) {
     console.log(backgroundImage);
     $('#bgLink').val(backgroundImage);
